885d6ad918253a93d0bb27aeae902b4f83f61714
Yashwanth <90147021+Yashwanth1119@users.noreply.github.com>
2025-06-18 07:05:13 +0000

    Initial commit

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ def run_pipeline():
 
 if __name__ == "__main__":
     run_pipeline()
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
--- pipeline/data_ingestion.py
+++ pipeline/data_ingestion.py
@@ -1,5 +1,5 @@
-import pandas as pd 
- 
- def load_csv(path: str) -> pd.DataFrame:
-    return pd.read_csv(path)
-    
+import pandas as pd
+
+def load_csv(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path)
+    return df
